54d1bc104d777a88ae7661498bde81971a7ff185
Sergio Rodriguez <44123331+Sergiorpo@users.noreply.github.com>
2022-04-23 13:27:23 +0200

    Correccion de fallos y agregadas mas preguntas

--- main.py
+++ main.py
@@ -5,58 +5,116 @@ print("Que empiece el juego. Elige la respuesta correcta y gana el maximo de pun
 
 puntuacion = 0
 respuesta = None
-
 ##########Primera pregunta.
 print("¿De que color es el cielo?\n"
       "A: Verde\n"
       "B: Azul\n"
       "C: Marron\n")
 
-while respuesta != "A" and respuesta != "a" and respuesta != "B" and respuesta != "b" and respuesta != "C" and respuesta != "c":
-    respuesta = input("Elige A,B o C")
+while respuesta != "A" and respuesta != "a" and  respuesta != "B" and respuesta != "b" and respuesta != "C" and respuesta != "c":
+    respuesta = input("Elige A,B o C\n")
 
 if respuesta == "A" or "a":
     puntuacion += 0
 
-elif respuesta == "B" or "b":
+if respuesta == "B" or "b":
     puntuacion += 1
 
 elif respuesta == "C" or "c":
     puntuacion += 0
 
 #########Segunda pregunta
-
-print("¿De qué colores es la bandera de México?\n"
+respuesta = None
+print("¿De que colores es la bandera de Mexico?\n"
       "A: amarillo, blanco y rojo\n"
       "B: verde, blanco y azul\n"
       "C: verde, blanco y rojo\n")
 
 while respuesta != "A" and respuesta != "a" and respuesta != "B" and respuesta != "b" and respuesta != "C" and respuesta != "c":
-    respuesta = input("Elige A,B o C")
+    respuesta = input("Elige A,B o C\n")
 
 if respuesta == "A" or "a":
     puntuacion += 0
 
-elif respuesta == "B" or "b":
+if respuesta == "B" or "b":
     puntuacion += 0
 
-elif respuesta == "C" or "c":
+if respuesta == "C" or "c":
     puntuacion += 1
 
 #########Tercera pregunta
-
-print("¿Cuándo acabó la II Guerra Mundial?\n"
+respuesta = None
+print("¿Cuando acabo la II Guerra Mundial?\n"
       "A: 1945\n"
       "B: 1942\n"
       "C: 1946\n")
 
 while respuesta != "A" and respuesta != "a" and respuesta != "B" and respuesta != "b" and respuesta != "C" and respuesta != "c":
-    respuesta = input("Elige A,B o C")
+    respuesta = input("Elige A,B o C\n")
 
 if respuesta == "A" or "a":
     puntuacion += 1
 
-elif respuesta == "B" or "b":
+if respuesta == "B" or "b":
+    puntuacion += 0
+
+if respuesta == "C" or "c":
+    puntuacion += 0
+
+#########Cuarta pregunta
+respuesta = None
+print("¿Como se denomina el resultado de la multiplicacion?\n"
+      "A: Parciales\n"
+      "B: Multiplicador\n"
+      "C: producto\n")
+
+while respuesta != "A" and respuesta != "a" and respuesta != "B" and respuesta != "b" and respuesta != "C" and respuesta != "c":
+    respuesta = input("Elige A,B o C\n")
+
+if respuesta == "A" or "a":
+    puntuacion += 0
+
+if respuesta == "B" or "b":
+    puntuacion += 0
+
+if respuesta == "C" or "c":
+    puntuacion += 1
+print("Ya queda poco.")
+
+
+#########Quinta pregunta
+respuesta = None
+print("¿Que año llego Cristobal Colon a America?\n"
+      "A: 1492\n"
+      "B: 1590\n"
+      "C: 1495\n")
+
+while respuesta != "A" and respuesta != "a" and respuesta != "B" and respuesta != "b" and respuesta != "C" and respuesta != "c":
+    respuesta = input("Elige A,B o C\n")
+
+if respuesta == "A" or "a":
+    puntuacion += 1
+
+if respuesta == "B" or "b":
+    puntuacion += 0
+
+elif respuesta == "C" or "c":
+    puntuacion += 0
+
+#########Sexta pregunta
+respuesta = None
+print("¿Cual es el color que representa la esperanza?\n"
+      "A: Verde\n"
+      "B: Blanco\n"
+      "C: Azul\n")
+
+while respuesta != "A" and respuesta != "a" and respuesta != "B" and respuesta != "b" and respuesta != "C" and respuesta != "c":
+    respuesta = input("Elige A,B o C\n")
+
+if respuesta == "A" or "a":
+    puntuacion += 1
+
+if respuesta == "B" or "b":
     puntuacion += 0
 
 elif respuesta == "C" or "c":
